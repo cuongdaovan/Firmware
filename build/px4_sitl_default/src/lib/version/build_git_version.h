aff9c0c95135c21adb8f9bb9d4784d00f9e6ac67
cuongdaovan <cuongdaovan262@gmail.com>
2019-06-13 11:10:41 +0700

    test

---
+++ build/px4_sitl_default/src/lib/version/build_git_version.h
@@ -0,0 +1,15 @@
+
+/* Auto Magically Generated file */
+/* Do not edit! */
+#pragma once
+
+#define PX4_GIT_VERSION_STR  "736a1c4778c8c8016cce7feab2e84ec8a0abc6bd"
+#define PX4_GIT_VERSION_BINARY 0x736a1c4778c8c801
+#define PX4_GIT_TAG_STR  "v1.9.0-218-g736a1c4778"
+#define PX4_GIT_BRANCH_NAME  "master"
+
+#define ECL_LIB_GIT_VERSION_STR  "0f49eb34a08e499a26d749ed981429047f2100ff"
+#define ECL_LIB_GIT_VERSION_BINARY 0x0f49eb34a08e499a
+
+#define MAVLINK_LIB_GIT_VERSION_STR  "70c1b4ef2db3244df23196bf2e47e369e2e8b78a"
+#define MAVLINK_LIB_GIT_VERSION_BINARY 0x70c1b4ef2db3244d
